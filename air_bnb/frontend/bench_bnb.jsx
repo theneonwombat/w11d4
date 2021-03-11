ac6ed0b405246789a4ddb1e58448182a8015e2ef
Alec Caro <alec.caro@gmail.com>
2021-03-11 15:04:36 -0500

    Mmm

---
+++ air_bnb/frontend/bench_bnb.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  debugger
+  const root = document.getElementById("root");
+  ReactDOM.render(<h1>It's Workin' Now!</h1>, root);
+})
